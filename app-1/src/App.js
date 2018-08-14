73cf3b1dd2add131cb81466557089daf98d525c3
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-08-13 17:23:43 -0700

    completed assessment

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      str: '',
+      
+    }
+  }
+
+  handleChange(val){
+    this.setState({str: value})
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input type= 'text' className= 'textBox' onChange= {(e) => this.handleChange(e.target.value)}/>  
+        <p>{this.state.str}</p>
       </div>
     );
   }
